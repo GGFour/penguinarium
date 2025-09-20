24a5b4246fdec69bd030194bbfa58de4edc70b40
pochapali <ivantrufan27@gmail.com>
2025-09-20 17:07:53 +0300

    Initial frontend commit

---
+++ frontend/dashboard.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from pages.alerts import show_alert_list
+
+st.set_page_config(
+    page_title="Tupic",
+    page_icon="🐧",
+)
+# if "selected_alert_id" in st.session_state:
+#     show_alert_detail(st.session_state["selected_alert_id"])
+# else:
+#     show_alert_list()
+# st.title("Dashboard")
+# st.sidebar.success("Select page")
+
+col1, col2 = st.columns(2)  # Split page into two equal columns
+
+with col1:
+    st.header("Left Side")
+    st.write("Data sources go here")
+
+with col2:
+    if not "selected_alert_id" in st.session_state:
+        st.session_state["selected_alert_id"] = None
+    show_alert_list()
---
+++ frontend/pages/alerts.py
@@ -0,0 +1,63 @@
+import streamlit as st
+
+# Example alerts data with details
+alerts = [
+    {
+        "id": 1,
+        "name": "Missing Values Detected",
+        "detail": "This alert occurs when your dataset contains missing or null values that need to be addressed."
+    },
+    {
+        "id": 2,
+        "name": "Duplicate Rows Found",
+        "detail": "This alert indicates that your data contains duplicate records."
+    },
+    {
+        "id": 3,
+        "name": "Outlier Values Detected",
+        "detail": "Detected data points that are significantly different from others, possibly indicating errors or special cases."
+    },
+    {
+        "id": 4,
+        "name": "Schema Change Alert",
+        "detail": "The structure of your dataset has changed, such as new or missing columns."
+    }
+]
+
+
+def set_selected_alert_id(id: int):
+    st.session_state["selected_alert_id"] = id
+
+
+def show_alert_list():
+    st.title("Data Anomaly Alerts")
+    st.write("Click on an alert to learn more about the issue.")
+    for alert in alerts:
+        st.button(alert["name"], key=alert["id"],
+                  on_click=alert_navigate(alert["id"]))
+
+
+def show_alert_detail(alert_id):
+    alert = next((a for a in alerts if a["id"] == alert_id), None)
+    if alert:
+        st.title(alert["name"])
+        st.write(alert["detail"])
+        st.button("Back to Alert List",
+                  on_click=alert_navigate(None))
+
+
+def alert_navigate(id: int):
+    st.session_state["selected_alert_id"] = id
+    if id is not None:
+        st.switch_page("pages/alert.py")
+    else:
+        st.switch_page("dashboard.py")
+
+
+def display_alerts_menu():
+    if not "selected_alert_id" in st.session_state:
+        st.session_state["selected_alert_id"] = None
+    if st.session_state["selected_alert_id"] is not None:
+        alert_navigate(st.session_state["selected_alert_id"])
+    else:
+        show_alert_list()
