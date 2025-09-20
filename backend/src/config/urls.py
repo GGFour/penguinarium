7c6557ed76720a5dfe81ed590f720ef2b9cb6b81
Dmitrii Malkov <dmitrii.malkov.unreal@gmail.com>
2025-09-20 21:30:34 +0300

    Added route data_soruce/id/tables

--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
     path('api/v1/', include('api.urls_v1')),
+    path('api/', include('api.urls')),
 ]
